b2159502dab885a0845c614a3f46ac90be95a060
Duy Manh <duymanh25102001@gmail.com>
2023-11-09 00:01:27 +0700

    click avatar user

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -2,17 +2,23 @@ import React from 'react'
 import { HStack, Image, chakra } from '@chakra-ui/react'
 import avatarDefault from '../assets/avatar-default.png'
 import { useAuth } from '../hooks/useReducerContext'
+import { useNavigate } from 'react-router-dom'
+import { path } from '../ultils/constant'
 
 
 const User = () => {
-
+    const navigate = useNavigate()
     const { user } = useAuth()
+
+    const handleClick = () => {
+        navigate(`${path.SYSTEM}/${path.PROFILE}`)
+    }
 
     // console.log(user)
     return (
         <HStack justifyContent={'flex-end'}>
-            <Image src={user?.avatar || avatarDefault} alt='avatar' borderRadius='full' boxSize='45px' />
-            <chakra.span> Xin chào! <chakra.span fontWeight='semibold'>{user?.name || user?.displayName}</chakra.span></chakra.span>
+            <Image onClick={handleClick} src={user?.avatar || avatarDefault} alt='avatar' borderRadius='full' boxSize='45px' cursor='pointer' />
+            <chakra.span onClick={handleClick} cursor='pointer'> Xin chào! <chakra.span fontWeight='semibold'>{user?.name || user?.displayName}</chakra.span></chakra.span>
         </HStack>
     )
 }
